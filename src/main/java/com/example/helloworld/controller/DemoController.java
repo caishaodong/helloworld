3572272692ff1d11018c1c234283e14584556835
caishaodong <1146631758@qq.com>
2018-09-21 17:00:24 +0800

    init project

---
+++ src/main/java/com/example/helloworld/controller/DemoController.java
@@ -0,0 +1,46 @@
+package com.example.helloworld.controller;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CountDownLatch;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    RateLimiter limiter = RateLimiter.create(10);
+
+    @RequestMapping("/demo1")
+    public String demo1() {
+        int count = 20;
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        for (int i = 0; i < count; i++) {
+            new Thread(() -> {
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                doRequest();
+            }, "t" + i).start();
+        }
+
+        countDownLatch.countDown();
+
+
+        return "success!";
+    }
+
+
+    private void doRequest() {
+        if (limiter.tryAcquire()) {
+            System.out.println(Thread.currentThread().getName() + "获取成功");
+        } else {
+            System.out.println(Thread.currentThread().getName() + "获取失败");
+        }
+    }
+
+
+}
